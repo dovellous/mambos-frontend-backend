7d04563240d2f3e6823c969f86521b924af3d8e8
Douglas Maposa <doug.maposa@gmail.com>
2023-03-08 17:12:24 +0200

    Cleaned up images

--- landing/src/app/components/layout/widgets/menu/MenuFoodItems.tsx
+++ landing/src/app/components/layout/widgets/menu/MenuFoodItems.tsx
@@ -1,65 +1,315 @@
 import React, {useEffect, useState} from "react";
-
-import { useSelector } from 'react-redux';
+// @ts-ignore
+import Modal from 'react-modal';
+import {useSelector} from 'react-redux';
 import Snippets from "../../../../helpers/Snippets";
 
+Modal.setAppElement('#app');
+
 export const MenuFoodItems: React.FunctionComponent = (): any | null => {
-    
+
     // @ts-ignore
     const menuCategories = useSelector(state => state.menu.value);
-    
+
     const getMenuSlug: Function = (label: string): string => {
-        
-        return label.toString().replace(' ','-').toLowerCase();
-        
+
+        return label.toString().replace(' ', '-').toLowerCase();
+
     }
-    
-    useEffect(()=>{
-        
-        console.warn("::::: --- MENU --- :::::", menuCategories);
-        
-    },[menuCategories]);
-    
+
+    const [selectedProductDetails, setSelectedProductDetails] = React.useState({
+        addons:{
+            gaap_addons_items: [],
+            gaap_sides_items: []
+        },
+        di_carb_g:"0.00",
+        di_carb_p:"0.00",
+        di_energy_c:"0.00",
+        di_energy_g:"0",
+        di_fat_g:"0.00",
+        di_fat_p:"0.00",
+        di_protein_g:"0.00",
+        di_protein_p:"0.00",
+        gaap_addon_code:null,
+        gaap_addons_cart_data:0,
+        gaap_addons_qty:0,
+        gaap_addons_total:0,
+        gaap_code:"0",
+        gaap_department_id:"0",
+        gaap_department_name:"",
+        gaap_department_slug:"value_meal",
+        gaap_forex_price:"0.00",
+        gaap_image:null,
+        gaap_is_active:"0",
+        gaap_local_price:"0.00",
+        gaap_sides_cart_data:0,
+        gaap_sides_qty:0,
+        gaap_sides_total:0,
+        product_added_by:"0",
+        product_added_on:null,
+        product_addons:"0",
+        product_addons_data:[],
+        product_allergens:"0",
+        product_barcode:null,
+        product_cart_qty:0,
+        product_code:"0",
+        product_code_old:null,
+        product_description:"",
+        product_except:null,
+        product_id:"1247",
+        product_image:null,
+        product_is_addon:"0",
+        product_is_customizable:"0",
+        product_is_deliverable:"0",
+        product_is_orderable:"0",
+        product_is_side:"0",
+        product_name:"",
+        product_preparation:"0",
+        product_price_local:"0.00",
+        product_price_regular:"0.00",
+        product_related:null,
+        product_requires_addon:"0",
+        product_requires_side:"0",
+        product_sales:"0",
+        product_sides:null,
+        product_updated_by:null,
+        product_updated_on:null,
+        product_views:"0"
+    })
+
+    const selectProduct: Function = (product: any): void => {
+
+        setSelectedProductDetails(product[1]);
+
+        console.log(":: PRODUCT ::", product[1]);
+
+        openModal();
+
+    }
+
+    const getProductDescription: Function = (product_description: string | null): string => {
+
+        if (product_description == null) {
+            return "...";
+        }
+        if (product_description.length > 105) {
+            return product_description.substring(0, 105) + '...';
+        }
+        return product_description;
+
+    }
+
+    const activeLink: Function = (anchor: any, anchors: any): void => {
+        anchors.forEach((item: any) => item.classList.remove('active'));
+        anchor.classList.add('active');
+    }
+
+    const [subtitle, setSubtitle] = React.useState('');
+
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const [customStyles, setCustomStyles] = React.useState({
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    });
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    useEffect(() => {
+
+        let section = document.querySelectorAll('div.container.menu-category');
+        let anchors = document.querySelectorAll('a.menu-item-link');
+
+        window.onscroll = () => {
+            section.forEach((sec: any) => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+                if (top >= offset && top < offset + height) {
+
+                    const targetSelector: any = `menu_item_link_${id}`;
+                    // @ts-ignore
+                    const target = window.document.getElementById(targetSelector);
+                    activeLink(target, anchors);
+                }
+            })
+        };
+
+    }, []);
+
     return (
         <>
-    
-            {menuCategories.products.map((category: any, categoryIndex: number) => (
-            
-            <div id={`section_${categoryIndex}_${category.category_slug}`} className="container menu-category" key={categoryIndex}>
-                
-                <div className={'menu-cat-banner'} style={{backgroundImage: `url(/assets/elements/banners/banner-${getMenuSlug(category.category_slug)}.svg)`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', minHeight: '250px'}}>
-                    <img src={`/assets/elements/banners/txt-${getMenuSlug(category.category_slug)}.svg`} className="category-banner-text-icon" />
 
-                    
-                    <div className='row menu-foot-cats'>
-                        {Snippets.objects.convertToArray(category.products).map((product:any, productIndex:number) => (
-                            <div className="col-xl-4 col-md-6" >
-                                <div className="card border-radius-zero border-0 shadow-sm mb-30" style={{backgroundImage: "url('/assets/bg/banners/food-card-top-wave.svg')", minHeight: '200px' }}>
-                                    <div className="card-body">
-                                        <h3 className="mb-3">{product[1].product_name}</h3>
-                                        <p className="line-height-24" dangerouslySetInnerHTML={{__html: product[1].product_description}}></p>
-                                        <div className="banner-strip-d-flex">
-                                            <div className="price-text">
-                                                {menuCategories.currency} ${menuCategories.currency === 'USD' ? product[1].gaap_forex_price : product[1].gaap_local_price}
-                                            </div>
-                                            <div>
-                                                <img src={`/assets/elements/banners/food-card-hearts.svg`} />
-                                            </div>
-                                        </div>
+            {menuCategories.products.map((category: any, categoryIndex: number) => (
+
+                <>
+
+                    <div id={`section_${category.category_slug}_${categoryIndex}`}
+                         className={`section_${category.category_slug}_${categoryIndex} container menu-category`}
+                         key={categoryIndex}>
+
+                        <div className={'menu-cat-banner'} style={{
+                            backgroundImage: `url(/assets/elements/banners/banner-${getMenuSlug(category.category_slug)}.svg)`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'contain',
+                            minHeight: '250px'
+                        }}>
+                            <img src={`/assets/elements/banners/txt-${getMenuSlug(category.category_slug)}.svg`}
+                                 className="category-banner-text-icon"/>
+                            <div className='row menu-foot-cats'>
+                                <div className="col-xl-4 col-md-6">
+                                    <div className="card border-radius-zero border-0 shadow-sm mb-30" style={{
+                                        backgroundImage: `url('/assets/elements/menu/placeholder.jpeg')`,
+                                        backgroundSize: 'cover',
+                                        backgroundColor: '#000000',
+                                        backgroundPosition: 'center center',
+                                        backgroundRepeat: 'no-repeat',
+                                        height: '270px',
+                                        paddingTop: '70px'
+                                    }}>
+                                        &nbsp;
                                     </div>
                                 </div>
+                                {Snippets.objects.convertToArray(category.products).map((product: any, productIndex: number) => (
+
+                                    <>
+
+                                        { parseFloat(product[1].gaap_forex_price) > 0 && (
+
+                                        <div className={`col-xl-4 col-md-6`} key={productIndex}>
+                                            <div className="card border-radius-zero border-0 shadow-sm mb-30" style={{
+                                                backgroundImage: `url('/assets/elements/menu/${category.category_slug}_${productIndex < 2 ? 'first' : 'last'}.svg')`,
+                                                backgroundSize: '50%',
+                                                backgroundPositionX: '30px',
+                                                backgroundRepeat: 'no-repeat',
+                                                height: '270px',
+                                                paddingTop: '70px'
+                                            }}>
+                                                <div className="card-body" onClick={(e) => {
+                                                    selectProduct(product)
+                                                }} style={{cursor: 'pointer'}}>
+                                                    <h3 className="mb-3">{product[1].product_name}</h3>
+                                                    <p className="line-height-24"
+                                                       dangerouslySetInnerHTML={{__html: getProductDescription(product[1].product_description)}}></p>
+                                                    <div className="banner-strip-d-flex mt-3">
+                                                        <div className="price-text">
+                                                            {menuCategories.currency} ${menuCategories.currency === 'USD' ? product[1].gaap_forex_price : product[1].gaap_local_price}
+                                                        </div>
+                                                        <div>
+                                                            <img src={`/assets/elements/banners/food-card-hearts.svg`}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        )}
+
+                                    </>
+
+                                ))}
+
                             </div>
-                        ))}
+
+                        </div>
+
                     </div>
-                
-                </div>
-            
-            </div>
-            
+
+
+                    {(categoryIndex % 3) === 0 && (
+
+                        <div
+                            className={`container w-100 cat-index-${categoryIndex} mod-${(categoryIndex % 3)} div-${(categoryIndex / 3)}`} style={{width: `100%`, margin: 'auto', paddingTop: '0px', paddingBottom: '120px'}}>
+
+                            <img src={`/assets/elements/banners/ad-${(categoryIndex / 3)}.png`}/>
+
+                        </div>
+
+                    )}
+
+                </>
+
             ))}
-        
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <div className="card border-radius-zero border-0 shadow-sm" style={{
+                    backgroundColor: '#ffffff',
+                    minHeight: '270px',
+                    padding: '20px'
+                }}>
+
+                    <img
+                        onError={({ currentTarget }) => {
+                            currentTarget.onerror = null; // prevents looping
+                            currentTarget.src="https://www.mamboschicken.com/uploads/products/food-1024/null.jpg";
+                        }}
+                        src={`https://www.mamboschicken.com/uploads/products/food-1024/${selectedProductDetails.product_code_old}.jpg`} className={'w-100'} />
+
+                    <h3 className="mb-3 mt-3">{selectedProductDetails.product_name}</h3>
+                    <p className="line-height-24"
+                       dangerouslySetInnerHTML={{__html: selectedProductDetails.product_description}}></p>
+
+                    {selectedProductDetails.di_carb_g !== "0.00" && (
+
+                    <table width="100%" className="tg mb-2 mt-2">
+                        <thead>
+                        <tr className="tg-0pky">
+                            <th className="tg-0pky">Energy</th>
+                            <th className="tg-0pky">Carbs</th>
+                            <th className="tg-0pky">Proteins</th>
+                            <th className="tg-0pky">Fat</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td className="tg-0pky">{selectedProductDetails.di_energy_c}Cal</td>
+                            <td className="tg-0pky">{selectedProductDetails.di_carb_g}g</td>
+                            <td className="tg-0pky">{selectedProductDetails.di_protein_g}</td>
+                            <td className="tg-0pky">{selectedProductDetails.di_fat_g}</td>
+                        </tr>
+                        <tr>
+                            <td className="tg-0pky"><small>*DI</small><br/>{selectedProductDetails.di_energy_c}%</td>
+                            <td className="tg-0pky"><small>*DI</small><br/>{selectedProductDetails.di_carb_p}%</td>
+                            <td className="tg-0pky"><small>*DI</small><br/>{selectedProductDetails.di_protein_p}%</td>
+                            <td className="tg-0pky"><small>*DI</small><br/>{selectedProductDetails.di_fat_p}%</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                    )}
+
+                    <div className="banner-strip-d-flex mt-3">
+                        <div className="price-text">
+                            USD ${selectedProductDetails.gaap_forex_price} or ZWD ${selectedProductDetails.gaap_local_price}
+                        </div>
+                        <div>
+                            <img src={`/assets/elements/banners/food-card-hearts.svg`}/>
+                        </div>
+                    </div>
+
+                </div>
+            </Modal>
+
         </>
-    
+
     );
-    
+
 };
--- landing/src/app/components/layout/widgets/menu/MenuNavBar.tsx
+++ landing/src/app/components/layout/widgets/menu/MenuNavBar.tsx
@@ -9,21 +9,10 @@ type MenuNavBarProps = {
 export const MenuNavBar: React.FunctionComponent<MenuNavBarProps> = ({children, ...props}): any | null => {
 	
 	const {menuCategories} = props;
-	
-	console.warn("::::::: CATS :::::::", menuCategories);
-	
+
 	const scrollToMenuSection: Function = (categoryItem: any, categoryItemIndex: number): void => {
 		
-		Snippets.elements.scrollTo(`section_${categoryItemIndex}_${categoryItem.category_slug}`);
-
-		let allItems:any = document.getElementsByClassName('menu-item-link');
-		for(let i:number = 0; i<allItems.length; i++){
-			allItems[i].classList.remove('active');
-		}
-		setTimeout(()=>{
-			let currentItem:any = document.getElementById(`menu_item_link_${categoryItem.category_slug}_${categoryItemIndex}`);
-			currentItem.classList.add('active');
-		},100)
+		Snippets.elements.scrollTo(`section_${categoryItem.category_slug}_${categoryItemIndex}`);
 
 	}
 
@@ -52,7 +41,7 @@ export const MenuNavBar: React.FunctionComponent<MenuNavBarProps> = ({children, 
 					<ul className='menu-nav-container'>
 						{menuCategories.map((categoryItem: any, categoryItemIndex: number) => (
 							<li className='menu-food-cat-item' key={categoryItemIndex}>
-								<a href='#.' id={`menu_item_link_${categoryItem.category_slug}_${categoryItemIndex}`}  className={`menu-item-link ${categoryItem.category_slug}-${categoryItemIndex}`} target='' onClick={(e) => {
+								<a href='#.' id={`menu_item_link_section_${categoryItem.category_slug}_${categoryItemIndex}`}  className={`menu-item-link ${categoryItem.category_slug}_${categoryItemIndex}`} target='_self' onClick={(e) => {
 									scrollToMenuSection(categoryItem, categoryItemIndex, this)
 								}}>
 									{categoryItem.category_name}
